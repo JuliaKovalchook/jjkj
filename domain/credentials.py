c88557b1c630bff5cd775d2ad67f5bab689da195
JuliaKovalchook <uliakovalchuk@gmail.comheroku>
2019-12-25 19:07:36 +0300

    Initial commit

---
+++ domain/credentials.py
@@ -0,0 +1,11 @@
+username = 'pfrpsguyrukwye'
+password = 'a6599480a3355fee968234b49b7f9df2db2cfcece606ac67e92dcecc47e98d09'
+hostname = 'ec2-174-129-254-223.compute-1.amazonaws.com'
+port = 5432
+database_name = 'd22pm2ppi6p3f8'
+
+# username = 'postgres'
+# password = 'Vladik99'
+# hostname = '127.0.0.1'
+# port = 5432
+# database_name = 'Data_Base_Laboratories'
